959d09a9e6390db425fdaef0571c5ffb6130a04a
smiledouble <sdouble@aliyun.com>
2019-02-01 19:45:40 +0800

    首页完成

--- src/main/java/com/ziyun/cloud/module/operator/controller/LoginController.java
+++ src/main/java/com/ziyun/cloud/module/operator/controller/LoginController.java
@@ -6,13 +6,14 @@ import com.ziyun.cloud.constant.ResultCode;
 import com.ziyun.cloud.module.operator.entity.Operator;
 import com.ziyun.cloud.module.operator.service.IOperatorService;
 import com.ziyun.cloud.module.operator.vo.OperatorVo;
-import com.ziyun.cloud.utils.WebUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author:chenxiaoshuang
@@ -35,21 +36,34 @@ public class LoginController {
 
     @RequestMapping(MODEL + "/doLogin")
     @ResponseBody
-    public BaseResult<?> doLogin(@RequestBody OperatorVo operatorVo) {
-        BaseResult<?> baseResult =null;
+    public BaseResult<?> doLogin(@RequestBody OperatorVo operatorVo, HttpServletRequest request) {
+        BaseResult<?> baseResult = null;
         if (null != operatorVo) {
             Operator operator = this.iOperatorService.getOperatorByIdAndPwd(operatorVo);
             if (null != operator) {
                 //登陆成功 存放到session中
-                WebUtils.getCurrentSession().setAttribute("operator", operatorVo);
+               request.getSession().setAttribute("operator",operator);
                 //跳转到index.html
-                baseResult = BaseResult.success("/index");
+                baseResult = BaseResult.success("/toIndex");
             } else {
-            throw new BaseException(ResultCode.FAIL);
+                throw new BaseException(ResultCode.FAIL);
             }
         }
         return baseResult;
     }
 
+    //跳转到首页
+    @RequestMapping("/toIndex")
+    public ModelAndView toIndex() {
+        ModelAndView view = new ModelAndView("/index");
+        return view;
+    }
+
+    //跳转到info界面去
+    @RequestMapping(MODEL + "/toInfo")
+    public ModelAndView toInfo() {
+        ModelAndView view = new ModelAndView("/info");
+        return view;
+    }
 
 }
