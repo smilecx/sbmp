77c87a31a318ba8a9ac727ca9e6c425dbb5fd71e
陈笑一 <13507278420@139.com>
2019-02-01 14:07:49 +0800

    mvcConfig

---
+++ src/main/java/com/ziyun/cloud/config/MvcConfig.java
@@ -0,0 +1,51 @@
+package com.ziyun.cloud.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author:chenxiaoyi
+ * @Date:2019/2/1 14:05
+ */
+
+
+public class MvcConfig implements WebMvcConfigurer {
+
+    /** 直接可访问的url */
+    public static String[] IGNORE_URLS = new String[] {
+            "/swagger-resources/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**",
+          "/**/**/*" };
+
+    @Autowired
+    private Interceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/**")
+                .excludePathPatterns(IGNORE_URLS);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/templates/**/*").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new DateFormatter("yyyy-MM-dd HH:mm:ss"));
+        registry.addFormatter(new DateFormatter("yyyy-MM-dd"));
+    }
+}
